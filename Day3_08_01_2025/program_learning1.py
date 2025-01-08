05d5258239cb2e066d43bc4a93996eda01ddd4eb
RajuSaiPainkra <rajusaipainkra007@gmail.com>
2025-01-08 20:15:13 +0530

    task : Reverse the string  done

---
+++ Day3_08_01_2025/program_learning1.py
@@ -0,0 +1,17 @@
+# task : Reverse the String using the python
+
+# Solution 
+# we can the Reverse the string using the python 
+# method 1 : by the help of for loop
+# method 2 : by the help of while loop
+# method 3 : by the help of reverse() function
+# method 4 : by the help of recursion() method
+# 
+# 
+# Here is my Solution : 
+
+greeting = "Hello, World!" # this is string and it's variable 
+
+print(greeting[-1::-1]) # this is output : !dlroW ,olleH
+
+# this is a method but i don't know the method.....
