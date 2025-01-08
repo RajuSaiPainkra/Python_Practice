472f50254b391b8974727512e3bef9b2718d3af0
RajuSaiPainkra <rajusaipainkra007@gmail.com>
2025-01-08 20:30:15 +0530

    task : Type conversion Done , List to string

---
+++ Day3_08_01_2025/program_learning3.py
@@ -0,0 +1,14 @@
+# task  : Type Conversion - List to string
+
+# solution : 
+lists = [1, 2, 3, 4, 5, 6]
+
+print(f"Here is lists : {lists}")
+print(f"Type of lists : {type(lists)}")
+
+# convert list to string
+
+strings = str(lists)
+
+print("Here is Strings : ", strings)
+print(f"Type of Strings : {type(strings)}")
