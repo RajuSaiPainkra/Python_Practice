3388519a29416446a53afa0976018e80c80d6597
RajuSaiPainkra <rajusaipainkra007@gmail.com>
2025-01-08 20:23:33 +0530

    task Done , input in python

---
+++ Day3_08_01_2025/program_learning2.py
@@ -0,0 +1,8 @@
+# task 2 : make a input in python
+
+# solution 
+
+name = input("Enter your Name : \n")
+roll_number = input("Enter Your Roll Number : \n")
+greet = "Hello, " + name + "\nHere is your Roll Number : " + str(roll_number)
+print(greet)
